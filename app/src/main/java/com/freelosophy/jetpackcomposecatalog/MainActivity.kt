8135e0c9cd28c6c1f387da66b6daf9adb5f6259f
SpBarber <santiago.perez@eticco.es>
2022-09-30 07:48:59 +0200

    Simple Dialog

--- app/src/main/java/com/freelosophy/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/freelosophy/jetpackcomposecatalog/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.freelosophy.jetpackcomposecatalog
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -10,13 +9,13 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.freelosophy.jetpackcomposecatalog.ui.components.MyAlertDialog
+import com.freelosophy.jetpackcomposecatalog.ui.components.MyCustomDialog
 import com.freelosophy.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,10 +38,10 @@ class MainActivity : ComponentActivity() {
                         ){
                             Text("Mostrar diálogo")
                         }
-                        MyAlertDialog(
+                        MyCustomDialog(
                             show = show,
                             onDismiss = { show = false },
-                            onConfirm = { Log.i("Alert", "click") }
+//                            onConfirm = { Log.i("Alert", "click") }
                         )
                     }
                 }
--- app/src/main/java/com/freelosophy/jetpackcomposecatalog/ui/components/Dialogs.kt
+++ app/src/main/java/com/freelosophy/jetpackcomposecatalog/ui/components/Dialogs.kt
@@ -1,13 +1,65 @@
 package com.freelosophy.jetpackcomposecatalog.ui.components
 
-import androidx.compose.foundation.layout.Column
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.freelosophy.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
+
+
+/**
+ * Para el MyAlertDialog
+ */
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            JetpackComposeCatalogTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+////                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    var show by remember { mutableStateOf(false) }
+//                    Box(
+//                        modifier = Modifier.fillMaxSize(),
+//                        contentAlignment = Alignment.Center
+//                    ) {
+//                        Button(
+//                            onClick = { show = true}
+//                        ){
+//                            Text("Mostrar diálogo")
+//                        }
+//                        MyAlertDialog(
+//                            show = show,
+//                            onDismiss = { show = false },
+//                            onConfirm = { Log.i("Alert", "click") }
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
 
 @Composable
 fun MyAlertDialog(
@@ -39,16 +91,101 @@ fun MyAlertDialog(
     }
 }
 
-//@Preview(
-//    showBackground = true,
-//    showSystemUi = true,
-//    device = Devices.PIXEL_XL
-//)
-//@Composable
-//fun MyDialogPreview() {
-//    JetpackComposeCatalogTheme {
-//        Column {
-//            MyAlertDialog(true)
-//        }
-//    }
-//}
+@Composable
+fun MySimpleCustomDialog(
+    show: Boolean,
+    onDismiss: () -> Unit
+) {
+    if (show) {
+        Dialog(
+            onDismissRequest = { onDismiss() },
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .padding(24.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Esto es un ejemplo")
+                Text(text = "Esto es un ejemplo")
+                Text(text = "Esto es un ejemplo")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyCustomDialog(
+    show: Boolean,
+    onDismiss: () -> Unit
+){
+    if (show) {
+        Dialog(
+            onDismissRequest = { onDismiss() },
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .padding(24.dp)
+                    .fillMaxWidth()
+            ) {
+                MyTitleDialog("Set backup account")
+                AccountItem(email = "ejemplo1@gmail.com", drawable = com.freelosophy.jetpackcomposecatalog.R.drawable.avatar)
+                AccountItem(email = "ejemplo2@gmail.com", drawable = com.freelosophy.jetpackcomposecatalog.R.drawable.avatar)
+                AccountItem(email = "Añadir una nueva cuenta", drawable = com.freelosophy.jetpackcomposecatalog.R.drawable.add)
+            }
+        }
+    }
+}
+
+@Composable
+fun AccountItem(email: String, @DrawableRes drawable: Int) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painterResource(id = drawable),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = email,
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun MyTitleDialog(title: String) {
+    Text(
+        text = title,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+        modifier = Modifier
+            .padding(12.dp)
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PIXEL_XL
+)
+@Composable
+fun MyDialogPreview() {
+    JetpackComposeCatalogTheme {
+        Column {
+
+        }
+    }
+}
