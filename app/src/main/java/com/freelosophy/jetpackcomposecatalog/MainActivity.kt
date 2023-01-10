22bcba457516e186823d128b86186fc856560592
SpBarber <santiago.perez@eticco.es>
2023-01-10 12:20:07 +0100

    Navigation

--- app/src/main/java/com/freelosophy/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/freelosophy/jetpackcomposecatalog/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.freelosophy.jetpackcomposecatalog
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.freelosophy.jetpackcomposecatalog.ui.components.*
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.freelosophy.jetpackcomposecatalog.model.Routes
+import com.freelosophy.jetpackcomposecatalog.ui.navigation.*
 import com.freelosophy.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
 
 
@@ -49,7 +53,6 @@ import com.freelosophy.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
  * Válido a partir de LazyColumn o RecyclerView en JetpackCompose
  */
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -58,8 +61,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val navigationController = rememberNavController()
+                    NavHost(
+                        navController = navigationController,
+                        startDestination = Routes.Screen1.route
+                    ) {
+                        composable(Routes.Screen1.route) { Screen1(navigationController) }
+                        composable(Routes.Screen2.route) { Screen2(navigationController) }
+                        composable(Routes.Screen3.route) { Screen3(navigationController) }
+                        composable(
+                            Routes.Screen4.route,
+                            arguments = listOf(navArgument("age") {
+                                type = NavType.IntType
+                            })
+                        ) { backStackEntry ->
+                            Screen4(
+                                navigationController,
+                                backStackEntry.arguments?.getInt("age") ?: 0
+                            )
+                        }
+                        composable(
+                            Routes.Screen5.route,
+                            arguments = listOf(navArgument("name") { defaultValue = "Pepe" })
+                        ) {
+                            Screen5(
+                                navigationController,
+                                it.arguments?.getString("name")
+                            )
+                        }
+                    }
 //                        SimpleRecyclerView()
-                    ScaffoldExample()
+//                    ScaffoldExample()
                 }
             }
         }
--- app/src/main/java/com/freelosophy/jetpackcomposecatalog/ui/components/Scaffold.kt
+++ app/src/main/java/com/freelosophy/jetpackcomposecatalog/ui/components/Scaffold.kt
@@ -1,16 +1,19 @@
 package com.freelosophy.jetpackcomposecatalog.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 @Composable
@@ -18,29 +21,132 @@ fun ScaffoldExample() {
     val scaffoldState = rememberScaffoldState()
     val coroutineScope = rememberCoroutineScope()
     Scaffold(
-        topBar = { MyTopAppBar {
-            coroutineScope.launch {
-                scaffoldState.snackbarHostState.showSnackbar("Has pulsado el $it")
-            }
-        }
+        topBar = {
+            MyTopAppBar(
+                onClickIcon = {
+                    coroutineScope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar("Has pulsado el $it")
+                    }
+                },
+                onClickDrawer = { coroutineScope.launch { scaffoldState.drawerState.open() } }
+                )
         },
-        scaffoldState = scaffoldState
+        scaffoldState = scaffoldState,
+        bottomBar = { MyBottomNavigation() },
+        floatingActionButton = { MyFab() },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = false,
+        drawerContent = { MyDrawer() { coroutineScope.launch { scaffoldState.drawerState.close() } } }
     ) {}
 }
 
+@Composable
+fun MyDrawer(onCloseDrawer: () -> Unit) {
+    Column(
+        Modifier
+            .padding(8.dp)
+    ) {
+        Text(
+            text = "Primera opción",
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp).clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Segunda opción",
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp).clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Tercera opción",
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp).clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Cuarta opción",
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp).clickable { onCloseDrawer() }
+        )
+    }
+}
 
 @Composable
-fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+fun MyFab() {
+    FloatingActionButton(
+        onClick = { },
+        backgroundColor = Color.Yellow, contentColor = Color.Black
+    ) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "añadir")
+    }
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember { mutableStateOf(0) }
+    BottomNavigation(
+        backgroundColor = Color.Red,
+        contentColor = Color.White
+    ) {
+        BottomNavigationItem(
+            selected = index == 0,
+            onClick = { index = 0 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Home,
+                    contentDescription = "Home"
+                )
+            },
+            label = { Text("Home") }
+        )
+        BottomNavigationItem(
+            selected = index == 1,
+            onClick = { index = 1 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Edit,
+                    contentDescription = "Editar"
+                )
+            },
+            label = { Text("Editar") }
+        )
+        BottomNavigationItem(
+            selected = index == 2,
+            onClick = { index = 2 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "Eliminar"
+                )
+            },
+            label = { Text("Eliminar") }
+        )
+        BottomNavigationItem(
+            selected = index == 3,
+            onClick = { index = 3 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = "Usuario"
+                )
+            },
+            label = { Text("Usuario") }
+        )
+    }
+}
+
+@Composable
+fun MyTopAppBar(
+    onClickIcon: (String) -> Unit,
+    onClickDrawer: () -> Unit
+) {
     TopAppBar(
         title = {
             Text("Mi primera toolbar")
         },
         backgroundColor = Color.Red,
-        contentColor = Color.Black,
+        contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = { onClickIcon("atrás") }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
+            IconButton(onClick = {
+                // onClickIcon("atrás")
+                onClickDrawer()
+            }) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
             }
         },
         actions = {
